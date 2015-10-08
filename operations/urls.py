b466303f494404649cf5320340d02702b8409e6c
gsvr <george@westerncapelabs.com>
2015-10-08 18:29:13 +0200

    update urls

--- operations/urls.py
+++ operations/urls.py
@@ -11,8 +11,4 @@ router.register(r'trainers', views.TrainerViewSet)
 # Additionally, we include login URLs for the browseable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/',
-        include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api-token-auth/',
-        'rest_framework.authtoken.views.obtain_auth_token')
 ]
