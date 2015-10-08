3aa22e227517a493521d362bb9c233b4dd09a9be
gsvr <george@westerncapelabs.com>
2015-10-08 15:48:08 +0200

    add tests for after migration

--- operations/tests.py
+++ operations/tests.py
@@ -6,8 +6,21 @@ from rest_framework import status
 from rest_framework.test import APIClient
 from rest_framework.authtoken.models import Token
 
+from .models import Trainer
 
-from .models import DummyModel
+USER_1_SIMPLE = {
+    "name": "Test User 1 Simple",
+    "msisdn": "+27820010001"
+}
+USER_2_DETAILED = {
+    "name": "Test User 2 Detailed",
+    "msisdn": "+27820020002",
+    "email": "user2@operations.com",
+    "extras": {
+        "id": "1234561111222",
+        "coffee": "black"
+    }
+}
 
 
 class APITestCase(TestCase):
@@ -29,6 +42,12 @@ class AuthenticatedAPITestCase(APITestCase):
         self.token = token.key
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
 
+    def _make_trainer(self, post_data=USER_1_SIMPLE):
+        response = self.client.post('/operations/trainer/',
+                                    json.dumps(post_data),
+                                    content_type='application/json')
+        return response
+
 
 class TestExampleAppHStore(AuthenticatedAPITestCase):
 
@@ -43,17 +62,20 @@ class TestExampleAppHStore(AuthenticatedAPITestCase):
                          "Status code on /auth/login was %s (should be 200)."
                          % request.status_code)
 
-    def test_create_dummy_model_data(self):
-        post_data = {
-            "product_code": "test_code",
-            "data": {'a': 'a', 'b': 2}
-        }
-        response = self.client.post('/operations/dummy/',
-                                    json.dumps(post_data),
-                                    content_type='application/json')
-
+    def test_create_trainer_simple(self):
+        response = self._make_trainer()
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        d = Trainer.objects.last()
+        self.assertEqual(d.name, "Test User 1 Simple")
+        self.assertEqual(d.msisdn, "+27820010001")
+        self.assertEqual(d.email, None)
+        self.assertEqual(d.extras, None)
 
-        d = DummyModel.objects.last()
-        self.assertEqual(d.product_code, 'test_code')
-        self.assertEqual(d.data, {'a': 'a', 'b': '2'})
+    def test_create_trainer_detailed(self):
+        response = self._make_trainer(post_data=USER_2_DETAILED)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        d = Trainer.objects.last()
+        self.assertEqual(d.name, "Test User 2 Detailed")
+        self.assertEqual(d.msisdn, "+27820020002")
+        self.assertEqual(d.email, "user2@operations.com")
+        self.assertEqual(d.extras, {"id": "1234561111222", "coffee": "black"})
