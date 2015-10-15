3fff50864b122279c8232225cfa01c70efb495c7
gsvr <george@westerncapelabs.com>
2015-10-15 14:54:31 +0200

    simplify trainer id

--- operations/models.py
+++ operations/models.py
@@ -9,7 +9,6 @@ class Trainer(models.Model):
     """
     People who run training events
     """
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=200)
     msisdn = models.CharField(max_length=20)
     email = models.EmailField(null=True, blank=True)
