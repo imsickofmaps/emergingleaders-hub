3030e02b83be4532c735e1c2efc1532788b24d0b
gsvr <george@westerncapelabs.com>
2015-10-19 17:30:49 +0200

    serialize user & group only once

--- trainings/views.py
+++ trainings/views.py
@@ -1,26 +1,7 @@
-from django.contrib.auth.models import User, Group
 from .models import Event
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from .serializers import (UserSerializer, GroupSerializer, EventSerializer)
-
-
-class UserViewSet(viewsets.ModelViewSet):
-
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class GroupViewSet(viewsets.ModelViewSet):
-
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
+from .serializers import (EventSerializer)
 
 
 class EventViewSet(viewsets.ModelViewSet):
